0c23bcf25c59f87bf198204727dcaa83328b0c94
Dorian Mayamba <dorian.mayamba@gmail.com>
2023-06-14 20:46:32 +0100

    added a banner

--- src/Pages/Car/Cars.tsx
+++ src/Pages/Car/Cars.tsx
@@ -1,10 +1,18 @@
 import { useEffect, useState } from "react";
 import '../../styles/cars.css';
 
-export default function Cars(){
-    return(
-        <div className="cars-box">
-            
-        </div>
+export default function Cars() {
+    return (
+        <section className="cars-box">
+            <div className="cars-banner">
+            </div>
+            <aside className="cars-filter">
+                <ul>
+                    <li>
+                        <button>this</button>
+                    </li>
+                </ul>
+            </aside>
+        </section>
     )
 }
